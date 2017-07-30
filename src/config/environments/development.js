88f2ac3d12fd6ce2c1617fe7316b34309f996424
alexnm <alex.n.moldovan@gmail.com>
2017-07-30 10:45:48 +0300

    refactoring session

--- src/config/environments/development.js
+++ src/config/environments/development.js
@@ -3,4 +3,5 @@ module.exports = {
     port: 1234, // change with development port
     mongoUrl: "mongodb://localhost:27017/projectDbName", // replace "projectDbName" with a proper db name
     logLevel: "debug", // can be chenged to error, warning, info, verbose or silly
+    secret: "superSuperSecret",
 };
