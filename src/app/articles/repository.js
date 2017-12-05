57bdf0f0b5f9f4d0b77225c8cef2dad30b4349ee
Daniela Calcaianu <daniela.calcaianu@Danielas-Mac-mini.local>
2017-12-05 11:02:03 +0200

    fixed authorId bug

--- src/app/articles/repository.js
+++ src/app/articles/repository.js
@@ -1,11 +1,13 @@
 const mongoose = require( "mongoose" );
 
 const Article = mongoose.model( "Article" );
+const User = mongoose.model( "User" );
 
 const createArticle = async ( user, data ) => {
+    const { id } = user;
     try {
         const article = new Article( data );
-        article.authorId = user.id;
+        article.authorId = await User.findOne( { id } );
         const query = await article.save();
         return query;
     } catch ( err ) { return err; }
